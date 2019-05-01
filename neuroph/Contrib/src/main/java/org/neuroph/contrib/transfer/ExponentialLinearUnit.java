b82d733d83266924f4faba99b17fee8cde6d283a
Arben Bakiu <arben.bakiu91@gmail.com>
2019-05-01 22:00:32 +0200

    add ExponentialLinearUnit activation function

---
+++ neuroph/Contrib/src/main/java/org/neuroph/contrib/transfer/ExponentialLinearUnit.java
@@ -0,0 +1,33 @@
+
+package org.neuroph.contrib.transfer;
+
+import org.neuroph.core.transfer.TransferFunction;
+
+/**
+ * 
+ * @see https://arxiv.org/pdf/1511.07289.pdf
+ */
+public class ExponentialLinearUnit extends TransferFunction {
+
+    double alpha = 1d; 
+    
+    public ExponentialLinearUnit(double alpha) {
+        this.alpha = alpha;
+    }
+    
+    @Override
+    public double getOutput(double net) {
+        if (net < 0d) {
+            return alpha * (Math.exp(net) - 1);
+        }
+        return net;
+    }
+
+    @Override
+    public double getDerivative(double net) {
+        if (net > 0d) {
+            return 1d;
+        }
+        return getOutput(net) + alpha;
+    }
+}
